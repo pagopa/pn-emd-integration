f6e57ab8b585013b47f0c3ba2635227096e705e8
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-02-10 19:09:14 +0100

    fix: updated pn-commons version and removed constant for Mil Client name

--- src/main/java/it/pagopa/pn/emd/integration/middleware/client/MilAuthClient.java
+++ src/main/java/it/pagopa/pn/emd/integration/middleware/client/MilAuthClient.java
@@ -1,9 +1,11 @@
 package it.pagopa.pn.emd.integration.middleware.client;
 
+import it.pagopa.pn.commons.log.PnLogger;
 import it.pagopa.pn.emd.integration.dto.AccessTokenRequestDto;
 import it.pagopa.pn.emd.integration.generated.openapi.msclient.milauth.model.AccessToken;
 import reactor.core.publisher.Mono;
 
 public interface MilAuthClient {
+    String CLIENT_NAME = PnLogger.EXTERNAL_SERVICES.MIL_AUTH;
     Mono<AccessToken> getAccessTokens(AccessTokenRequestDto accessTokenRequestDto);
 }
