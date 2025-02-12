37726a460a3c76267fb358fcc5404c1dd18c13db
ECARADM5R <matteo.caradonna@emeal.nttdata.com>
2025-02-12 16:14:34 +0100

    PN-13877 - fixed test

--- src/main/java/it/pagopa/pn/emd/integration/utils/Utils.java
+++ src/main/java/it/pagopa/pn/emd/integration/utils/Utils.java
@@ -3,10 +3,12 @@ package it.pagopa.pn.emd.integration.utils;
 public class Utils {
 
     public static String removePrefix(String input) {
-        if (input.startsWith("PG-")) {
-            return input.substring(3);
-        } else if (input.startsWith("PF-")) {
-            return input.substring(3);
+        if (input != null) {
+            if (input.startsWith("PG-")) {
+                return input.substring(3);
+            } else if (input.startsWith("PF-")) {
+                return input.substring(3);
+            }
         }
         return input;
     }
