538faafc18ba99c0dc1a5d7f797e69d1dfd75cc2
mdisco-dgs <marco.disco@dgsspa.com>
2025-10-30 11:58:11 +0100

    PN-17043: add detail exception for amount parameter, removed unusued imports in EmdCoreServiceImpl.java

--- src/main/java/it/pagopa/pn/emd/integration/config/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
+++ src/main/java/it/pagopa/pn/emd/integration/config/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
@@ -1,14 +1,47 @@
 package it.pagopa.pn.emd.integration.config.springbootcfg;
 
-
+import it.pagopa.pn.common.rest.error.v1.dto.Problem;
 import it.pagopa.pn.commons.exceptions.ExceptionHelper;
 import it.pagopa.pn.commons.exceptions.PnResponseEntityExceptionHandler;
+import lombok.CustomLog;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ServerWebInputException;
+import reactor.core.publisher.Mono;
+import java.util.ArrayList;
+import java.util.List;
 
+@CustomLog
 @org.springframework.web.bind.annotation.ControllerAdvice
 @Import(ExceptionHelper.class)
 public class PnResponseEntityExceptionHandlerActivation extends PnResponseEntityExceptionHandler {
+
+    private final ExceptionHelper exceptionHelper;
+
     public PnResponseEntityExceptionHandlerActivation(ExceptionHelper exceptionHelper) {
         super(exceptionHelper);
+        this.exceptionHelper = exceptionHelper;
     }
+
+    @ExceptionHandler(ServerWebInputException.class)
+    public Mono<ResponseEntity<Problem>> handleServerWebInputException(ServerWebInputException ex) {
+        log.warn("ServerWebInputException caught: {}", ex.getMessage(), ex);
+        Problem problem = exceptionHelper.handleException(ex);
+
+        if (ex.getReason() != null && ex.getReason().toLowerCase().contains("type mismatch")) {
+
+            List<it.pagopa.pn.common.rest.error.v1.dto.ProblemError> errors = new ArrayList<>();
+            errors.add(new it.pagopa.pn.common.rest.error.v1.dto.ProblemError(
+                    "PN_GENERIC_INVALIDPARAMETER_PATTERN",
+                    ex.getMostSpecificCause().getMessage(),
+                    ex.getCause().getMessage()));
+            problem.setErrors(errors);
+
+        }
+
+        return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problem));
+    }
+
 }
