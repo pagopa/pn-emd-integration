a7c7b26aabf35d5c586d568c443cde3ae706f7e5
Riccardo Pompili <131166836+RiccardoPompili@users.noreply.github.com>
2025-10-31 14:53:34 +0100

    PN-17036 - WI 2.3 - Updating PnEmdIntegrationConfigs (#58)
    * PN-17036 - WI 2.3 - Updating PnEmdIntegrationConfigs
    
    * PN-17036 - WI 2.3 - fix log
    
    * PN-17036 - WI 2.3 - fix content analog message
    
    * PN-17036 - WI 2.3 - fix content analog message
    
    * PN-17036 - WI 2.3 - fix content analog message
    
    * PN-17036: Moved fetch template methods in Utils class. Added tests.
    
    * PN-17036 - WI 2.3 - fix exceptions name
    
    * PN-17036 - WI 2.3 - fix exceptions value
    
    ---------
    
    Co-authored-by: michelescara <michele.scaramuzzino@dgsspa.com>

--- src/main/java/it/pagopa/pn/emd/integration/config/PnEmdIntegrationConfigs.java
+++ src/main/java/it/pagopa/pn/emd/integration/config/PnEmdIntegrationConfigs.java
@@ -2,15 +2,20 @@ package it.pagopa.pn.emd.integration.config;
 
 import it.pagopa.pn.commons.conf.SharedAutoConfiguration;
 import it.pagopa.pn.emd.integration.cache.RedisMode;
+import lombok.CustomLog;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import javax.annotation.PostConstruct;
 import java.time.Duration;
+
+import static it.pagopa.pn.emd.integration.utils.Utils.fetchTemplate;
 
 @Configuration
 @ConfigurationProperties( prefix = "pn.emd-integration")
+@CustomLog
 @Data
 @Import({SharedAutoConfiguration.class})
 public class PnEmdIntegrationConfigs {
@@ -28,6 +33,8 @@ public class PnEmdIntegrationConfigs {
 
     private String courtesyMessageContent;
 
+    private MessagesTemplate msgsTemplate;
+
     @Data
     public static class CacheConfigs {
         private String hostName;
@@ -37,4 +44,23 @@ public class PnEmdIntegrationConfigs {
         private String cacheRegion;
         private RedisMode mode;
     }
+
+    @Data
+    public static class MessagesTemplate {
+        private String contentAnalogMsg;
+        private String contentDigitalMsg;
+        private String headerAnalogMsg;
+        private String headerDigitalMsg;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.msgsTemplate = new MessagesTemplate();
+        this.msgsTemplate.contentAnalogMsg = fetchTemplate("message_templates/content_analog_message.md");
+        this.msgsTemplate.contentDigitalMsg = fetchTemplate("message_templates/content_digital_message.md");
+        this.msgsTemplate.headerAnalogMsg = fetchTemplate("message_templates/header_analog_message.md");
+        this.msgsTemplate.headerDigitalMsg = fetchTemplate("message_templates/header_digital_message.md");
+        log.info("Messages templates loaded successfully.");
+    }
+
 }
