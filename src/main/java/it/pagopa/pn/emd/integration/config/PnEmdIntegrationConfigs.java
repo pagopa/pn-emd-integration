5b79fd77f49ad3dd6b0ef161ecc9e6f2165b4243
mdisco-dgs <137182394+mdisco-dgs@users.noreply.github.com>
2025-11-13 12:48:20 +0100

    Feature/pn 17034 (#63)
    * PN-17034: Updated OpenAPI with new fields.
    
    * PN-17034:
    - update emd-core-client.yaml and sendMessageRequestMap in EmdCoreServiceImpl;
    - add message creation from .md templates;
    - add error handling for missing or invalid analogSchedulingDate;
    - update unit tests
    
    * PN-17034: add handling for schedulingAnalogTime, add logs
    
    * PN-17034: add new error for generic BAD REQUEST, add workflow type in mapping sendMessageRequest
    
    * PN-17034: add oAuth2 in path sendMessage
    
    * PN-17034: restored x-field-extra-annotation for recipientId
    
    * PN-17034: Changed title example in emd-core-client.yaml.
    
    * PN-17034: restored barerAuth, handle fileName for CourtesyMessage
    
    * PN-17034: add constant for message templates base path, refactor code for send courtesy message
    
    * PN-17034: update EmdCoreServiceImplTest.java
    
    ---------
    
    Co-authored-by: michelescara <michele.scaramuzzino@dgsspa.com>

--- src/main/java/it/pagopa/pn/emd/integration/config/PnEmdIntegrationConfigs.java
+++ src/main/java/it/pagopa/pn/emd/integration/config/PnEmdIntegrationConfigs.java
@@ -7,10 +7,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
 import javax.annotation.PostConstruct;
 import java.time.Duration;
-
+import static it.pagopa.pn.emd.integration.utils.PnEmdIntegrationCostants.MESSAGE_TEMPLATES_BASE_PATH;
 import static it.pagopa.pn.emd.integration.utils.Utils.fetchTemplate;
 
 @Configuration
@@ -33,7 +32,7 @@ public class PnEmdIntegrationConfigs {
 
     private String courtesyMessageContent;
 
-    private MessagesTemplate msgsTemplate;
+    private Templates msgsTemplates;
 
     @Data
     public static class CacheConfigs {
@@ -46,21 +45,47 @@ public class PnEmdIntegrationConfigs {
     }
 
     @Data
-    public static class MessagesTemplate {
-        private String contentAnalogMsg;
-        private String contentDigitalMsg;
-        private String headerAnalogMsg;
-        private String headerDigitalMsg;
+    public static class CourtesyMessageTemplate {
+        private String header;
+        private String headerFileName;
+        private String content;
+        private String contentFileName;
+    }
+
+
+    @Data
+    public static class Templates {
+
+        private CourtesyMessageTemplate analogMsg;
+        private CourtesyMessageTemplate digitalMsg;
+
     }
 
     @PostConstruct
     public void init() {
-        this.msgsTemplate = new MessagesTemplate();
-        this.msgsTemplate.contentAnalogMsg = fetchTemplate("message_templates/content_analog_message.md");
-        this.msgsTemplate.contentDigitalMsg = fetchTemplate("message_templates/content_digital_message.md");
-        this.msgsTemplate.headerAnalogMsg = fetchTemplate("message_templates/header_analog_message.md");
-        this.msgsTemplate.headerDigitalMsg = fetchTemplate("message_templates/header_digital_message.md");
+        this.msgsTemplates = new Templates();
+
+        CourtesyMessageTemplate analog = new CourtesyMessageTemplate();
+        CourtesyMessageTemplate digital = new CourtesyMessageTemplate();
+
+        //initialize analog message template
+        analog.setHeaderFileName("header_analog_message.md");
+        analog.setContentFileName("content_analog_message.md");
+        analog.setHeader(fetchTemplate(MESSAGE_TEMPLATES_BASE_PATH + analog.getHeaderFileName()));
+        analog.setContent(fetchTemplate(MESSAGE_TEMPLATES_BASE_PATH + analog.getContentFileName()));
+
+        //initialize digital message template
+        digital.setHeaderFileName("header_digital_message.md");
+        digital.setContentFileName("content_digital_message.md");
+        digital.setContent(fetchTemplate(MESSAGE_TEMPLATES_BASE_PATH + digital.getContentFileName()));
+        digital.setHeader(fetchTemplate(MESSAGE_TEMPLATES_BASE_PATH + digital.getContentFileName()));
+
+        //assign templates to msgsTemplates
+        this.msgsTemplates.setAnalogMsg(analog);
+        this.msgsTemplates.setDigitalMsg(digital);
+
         log.info("Messages templates loaded successfully.");
+
     }
 
 }
--- src/main/java/it/pagopa/pn/emd/integration/config/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
+++ src/main/java/it/pagopa/pn/emd/integration/config/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
@@ -1,14 +1,47 @@
 package it.pagopa.pn.emd.integration.config.springbootcfg;
 
-
+import it.pagopa.pn.common.rest.error.v1.dto.Problem;
 import it.pagopa.pn.commons.exceptions.ExceptionHelper;
 import it.pagopa.pn.commons.exceptions.PnResponseEntityExceptionHandler;
+import lombok.CustomLog;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ServerWebInputException;
+import reactor.core.publisher.Mono;
+import java.util.ArrayList;
+import java.util.List;
+import it.pagopa.pn.emd.integration.exceptions.PnEmdIntegrationExceptionCodes;
 
+@CustomLog
 @org.springframework.web.bind.annotation.ControllerAdvice
 @Import(ExceptionHelper.class)
 public class PnResponseEntityExceptionHandlerActivation extends PnResponseEntityExceptionHandler {
+
+    private final ExceptionHelper exceptionHelper;
+
     public PnResponseEntityExceptionHandlerActivation(ExceptionHelper exceptionHelper) {
         super(exceptionHelper);
+        this.exceptionHelper = exceptionHelper;
+    }
+
+    @ExceptionHandler(ServerWebInputException.class)
+    public Mono<ResponseEntity<Problem>> handleServerWebInputException(ServerWebInputException ex) {
+        log.warn("ServerWebInputException caught: {}", ex.getMessage(), ex);
+        Problem problem = exceptionHelper.handleException(ex);
+
+        if (ex.getReason() != null) {
+
+            List<it.pagopa.pn.common.rest.error.v1.dto.ProblemError> errors = new ArrayList<>();
+            errors.add(new it.pagopa.pn.common.rest.error.v1.dto.ProblemError(
+                    PnEmdIntegrationExceptionCodes.PN_EMD_GENERIC_ERROR_BAD_REQUEST,
+                    ex.getMostSpecificCause().getMessage(),
+                    ex.getCause().getMessage()));
+            problem.setErrors(errors);
+
+        }
+
+        return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problem));
     }
 }
