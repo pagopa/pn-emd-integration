6fb2f762c6005a9321a0cb47f9f8dd8c17d38655
michelescara <michele.scaramuzzino@dgsspa.com>
2025-11-27 12:43:33 +0100

    PN-17331: Fixes. - Updated EmdClientV1 logs to distinguish between old and new versions. - Fixed EnableApiV2 type in microservice.yml.

--- src/main/java/it/pagopa/pn/emd/integration/middleware/client/EmdClientV1.java
+++ src/main/java/it/pagopa/pn/emd/integration/middleware/client/EmdClientV1.java
@@ -8,8 +8,8 @@ import reactor.core.publisher.Mono;
 
 public interface EmdClientV1 {
     String CLIENT_NAME = PnLogger.EXTERNAL_SERVICES.EMD_CORE;
-    String SUBMIT_MESSAGE_METHOD = "submitMessage";
-    String GET_RETRIEVAL_METHOD = "getRetrieval";
+    String SUBMIT_MESSAGE_METHOD = "submitMessageV1";
+    String GET_RETRIEVAL_METHOD = "getRetrievalV1";
     Mono<InlineResponse200> submitMessage(SendMessageRequest request, String token, String requestId);
     Mono<RetrievalResponseDTO> getRetrieval(String retrievalId, String accessToken);
 }
