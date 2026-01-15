9131729782fe08d3c29a755e34870cf2b8121d6c
riccardo.pompili <riccardo.pompili@dgsspa.com>
2026-01-15 16:05:10 +0100

    PN-17870 - Rimozione API obsoleta (V1)

--- src/test/java/it/pagopa/pn/emd/integration/service/EmdRetrievalServiceImplTest.java
+++ src/test/java/it/pagopa/pn/emd/integration/service/EmdRetrievalServiceImplTest.java
@@ -18,7 +18,6 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
-import java.util.Objects;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
