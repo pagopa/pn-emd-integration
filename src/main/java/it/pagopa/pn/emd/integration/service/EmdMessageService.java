40e542595d78f052a733032dbcb262947ba8c1bf
michelescara <79908782+michelescara@users.noreply.github.com>
2026-03-30 11:41:58 +0200

    PN-17482: migration to java21 and SPRINGBOOT3 (#86) (#87)
    * PN-17482: migration to java21 and SPRINGBOOT3 (#86)
    
    * PN-17482: migration to java21 and SPRINGBOOT3
    
    * PN-17482: restored x-field-extra-annotation and deleted the #NO EXTERNAL comment
    
    * PN-17482: update tests and pom.xml
    
    * fix
    
    * PN-16197: Resolve merge issues
    
    * PN-16197: Fix Copilot issues
    
    * PN-16197: update pn-commons version
    
    * PN-16197: fix pn-commons version
    
    ---------
    
    Co-authored-by: mdisco-dgs <137182394+mdisco-dgs@users.noreply.github.com>
    Co-authored-by: riccardo.pompili <riccardo.pompili@dgsspa.com>

--- src/main/java/it/pagopa/pn/emd/integration/service/EmdMessageService.java
+++ src/main/java/it/pagopa/pn/emd/integration/service/EmdMessageService.java
@@ -1,9 +1,9 @@
 package it.pagopa.pn.emd.integration.service;
 
-import it.pagopa.pn.emdintegration.generated.openapi.msclient.emdcoreclient.model.InlineResponse200;
+import it.pagopa.pn.emdintegration.generated.openapi.msclient.emdcoreclient.model.SubmitMessage200Response;
 import it.pagopa.pn.emdintegration.generated.openapi.server.v1.dto.SendMessageRequestBody;
 import reactor.core.publisher.Mono;
 
 public interface EmdMessageService {
-    Mono<InlineResponse200> submitMessage(SendMessageRequestBody request);
+    Mono<SubmitMessage200Response> submitMessage(SendMessageRequestBody request);
 }
