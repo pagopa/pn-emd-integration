0765f909327fb46df17d0f40dde1d162da60a87a
michele.scaramuzzino <michele.scaramuzzino@gmail.com>
2026-04-15 14:45:25 +0200

    PN-19388: reintroduce @SpringBootApplication

--- src/main/java/it/pagopa/pn/emd/integration/PnEmdIntegrationApplication.java
+++ src/main/java/it/pagopa/pn/emd/integration/PnEmdIntegrationApplication.java
@@ -3,8 +3,10 @@ package it.pagopa.pn.emd.integration;
 import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SpringBootApplication
 @EnableScheduling
 public class PnEmdIntegrationApplication {
     public static void main(String[] args) {
